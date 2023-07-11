c888cbc59f249f466b07ce6b144155838a8ec02e
unknown <cveta.dobreva@abv.bg>
2023-07-11 20:17:39 +0300

    init firebase

---
+++ ca/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBpgQdazerfyFO1zJhMT6kql9u4P0pe52I",
+        authDomain: "ca-project-c38ea.firebaseapp.com",
+        projectId: "ca-project-c38ea",
+        storageBucket: "ca-project-c38ea.appspot.com",
+        messagingSenderId: "907458614623",
+        appId: "1:907458614623:web:b2d040bb0de48f55ed12ea",
+        measurementId: "G-DZ8X7B26H0"
+    },
+    
+};
